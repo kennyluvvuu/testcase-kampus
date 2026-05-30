612bdf967f842269f34a20b4028274b3d3492307
Sergey Sokolovsky <sergey270307@gmail.com>
2026-05-30 17:54:26 +0700

    feat: add basic security headers and setup swagger
    - add CORS
    - add helmet plugin
    - add swagger with zod compatability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  });
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Тестовое задание: backend для ToDo')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(document));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
