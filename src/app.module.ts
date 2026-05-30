ab01220aed9e70b76a1ce5f441ea7925431aa94f
Sergey Sokolovsky <sergey270307@gmail.com>
2026-05-30 16:47:32 +0700

    feat: add database module with mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
