57abae11b5ff1a28d490beab1b46f5c2c1a4c824
Sergey Sokolovsky <sergey270307@gmail.com>
2026-05-31 17:16:32 +0700

    feat: add jwt auth service and contoller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserRequestDto, LoginUserResponseDto } from './dto/login.dto';
+import { ZodSerializerDto } from 'nestjs-zod';
+import {
+  RegisterUserRequestDto,
+  RegisterUserResponseDto,
+} from './dto/register.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ZodSerializerDto(LoginUserResponseDto)
+  @HttpCode(200)
+  @ApiOkResponse({
+    description: 'User logged in successfully',
+    type: LoginUserResponseDto,
+  })
+  async login(@Body() loginDto: LoginUserRequestDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('register')
+  @ZodSerializerDto(RegisterUserResponseDto)
+  @ApiCreatedResponse({
+    description: 'User registered successfully',
+    type: RegisterUserResponseDto,
+  })
+  async register(@Body() registerDto: RegisterUserRequestDto) {
+    return this.authService.register(registerDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConflictException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { RegisterUserRequest, RegisterUserResponse } from './dto/register.dto';
+import { LoginUserRequest, LoginUserResponse } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(user: RegisterUserRequest): Promise<RegisterUserResponse> {
+    const existingUser = await this.usersService.findByEmail(user.email);
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+    const createdUser = await this.usersService.createOne(user);
+    const token = await this.jwtService.signAsync({ userId: createdUser.id });
+    return { ...createdUser, token };
+  }
+
+  async login(user: LoginUserRequest): Promise<LoginUserResponse> {
+    const [isValid, userId] = await this.usersService.verifyPasswordByEmail(
+      user.email,
+      user.password,
+    );
+    if (!isValid) {
+      throw new ConflictException('Invalid credentials');
+    }
+    const token = await this.jwtService.signAsync({ userId });
+    return { token };
+  }
+}
